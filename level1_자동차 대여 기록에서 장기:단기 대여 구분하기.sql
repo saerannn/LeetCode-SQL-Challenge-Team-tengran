dcccdd8083c2172d5b75afd51af2573bca68fe63
박태은 <ptaeeun0202@gmail.com>
2023-05-12 16:31:21 +0900

    test

---
+++ level1_자동차 대여 기록에서 장기:단기 대여 구분하기.sql
@@ -0,0 +1,14 @@
+# CAR_RENTAL_COMPANY_RENTAL_HISTORY 테이블
+# 대여 시작일이 2022년 9월에 속하는 대여 기록
+# 대여 기간이 30일 이상이면 '장기 대여' 그렇지 않으면 '단기 대여' 로 표시하는 컬럼(컬럼명: RENT_TYPE)을 추가
+# 대여 기록 ID를 기준으로 내림차순 정렬
+
+SELECT HISTORY_ID
+     , CAR_ID
+     , DATE_FORMAT(START_DATE, "%Y-%m-%d") AS START_DATE
+     , DATE_FORMAT(END_DATE, "%Y-%m-%d") AS END_DATE
+     , CASE WHEN TIMESTAMPDIFF(DAY, START_DATE, END_DATE)+1 >= 30 THEN "장기 대여"
+       ELSE "단기 대여" END AS RENT_TYPE 
+FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
+WHERE DATE_FORMAT(START_DATE, "%Y-%m")="2022-09"
+ORDER BY HISTORY_ID DESC
