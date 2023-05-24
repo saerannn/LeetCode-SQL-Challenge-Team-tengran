7fe61b344c687394e9e64ce5adec683e25bfc0eb
박태은 <ptaeeun0202@gmail.com>
2023-05-24 16:03:43 +0900

    LEFT JOIN

---
+++ Join/1978. Employees Whose Manager Left the Company_tengny.sql
@@ -0,0 +1,11 @@
+WITH less AS (
+    SELECT *
+    FROM Employees
+    WHERE salary < 30000)
+
+SELECT less.employee_id
+FROM less
+    LEFT JOIN Employees ON less.manager_id=Employees.employee_id
+WHERE Employees.employee_id IS NULL 
+AND less.manager_id IS NOT NULL
+ORDER BY 1
