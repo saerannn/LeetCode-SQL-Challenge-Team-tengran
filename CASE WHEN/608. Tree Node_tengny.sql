a0487b9124eafb9a9fdd4ef1875698d607319187
박태은 <ptaeeun0202@gmail.com>
2023-06-07 16:37:20 +0900

    CASE WHEN, Node그림

---
+++ CASE WHEN/608. Tree Node_tengny.sql
@@ -0,0 +1,5 @@
+SELECT id
+      , CASE WHEN p_id IS NULL THEN "Root"
+             WHEN p_id IS NOT NULL AND id IN (SELECT DISTINCT p_id FROM Tree) THEN "Inner"
+             ELSE "Leaf" END AS type
+FROM Tree
